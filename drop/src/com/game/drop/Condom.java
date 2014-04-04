3703b99670c919c7903cf99cd871acfcedfff53f
eliotik <eliotik@gmail.com>
2014-04-04 10:54:17 +0300

    init

---
+++ drop/src/com/game/drop/Condom.java
@@ -0,0 +1,10 @@
+package com.game.drop;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Condom {
+	Rectangle box = new Rectangle();
+	Sprite sprite = new Sprite();
+	int speed = 200;
+}
---
+++ drop/src/com/game/drop/Sperm.java
@@ -0,0 +1,11 @@
+package com.game.drop;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Sperm {
+	Rectangle box = new Rectangle();
+	Sprite sprite = new Sprite();
+	int speed = 200;
+	int currentSpriteIndex = 1;
+}
